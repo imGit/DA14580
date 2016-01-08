61b876d0cc53f188032982b5836791e2ba8dafb5
Sam <gsxbeyond@sina.com>
2016-01-08 22:14:44 +0800

    Deleted keil_project folder

--- DA14580_RUGHWATCH_1.4.2/dk_apps/keil_projects/wearable/rughwatch/out/soft_delay.d
+++
@@ -1,4 +0,0 @@
-.\out\soft_delay.o: ..\..\..\src\plf\refip\src\user_func\soft_delay.c
-.\out\soft_delay.o: da14580_config.h
-.\out\soft_delay.o: da14580_stack_config.h
-.\out\soft_delay.o: ..\..\..\src\plf\refip\src\user_func\soft_delay.h
